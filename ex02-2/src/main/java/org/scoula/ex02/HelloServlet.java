3a1a37c4749e774ca7525f451378f182cb833365
Whatdoyumin <lym4963@naver.com>
2025-05-29 14:24:23 +0900

    [2일차] JSP 실습

---
+++ ex02-2/src/main/java/org/scoula/ex02/HelloServlet.java
@@ -0,0 +1,32 @@
+package org.scoula.ex02;
+
+import java.io.*;
+import javax.servlet.ServletException;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+
+@WebServlet(name = "helloServlet", urlPatterns = {"/xxx", "/yyy"})
+public class HelloServlet extends HttpServlet {
+    private String message;
+
+    @Override
+    public void init() throws ServletException {
+        System.out.println("init호출");
+    }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+
+        // Hello
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println("<h1>" + message + "</h1>");
+        out.println("</body></html>");
+    }
+
+    @Override
+    public void destroy() {
+        System.out.println("destroy 호출");
+    }
+
+}
---
+++ ex02-2/src/main/webapp/index.jsp
@@ -0,0 +1,18 @@
+<%@ page contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>JSP - Hello World</title>
+    </head>
+    <body>
+        <h1><%= "Hello World!" %>
+        </h1>
+        <br/>
+        <a href="hello-servlet">Hello Servlet</a>
+
+        <hr>
+        <a href="xxx">MyServelt 연결 1</a> <br>
+        <a href="yyy">MyServelt 연결 2</a> <br>
+        <a href="asdf">MyServelt 연결 3</a> <br>
+    </body>
+</html>
